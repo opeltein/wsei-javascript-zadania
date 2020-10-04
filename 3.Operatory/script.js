23de4edc8a1d68568f7847b2d0b5d642f949ffc7
opeltein <61658879+opeltein@users.noreply.github.com>
2020-10-04 13:39:24 +0200

    Update script.js

---
+++ 3.Operatory/script.js
@@ -0,0 +1,52 @@
+//Zadanie 1 
+
+let isEmpty = false;
+let isGold = true;
+
+console.log(isEmpty == isGold);
+
+//Zadanie 2
+
+let firstVariable = 4;
+let secondVariable = 3;
+let moduloResult = 0;
+
+moduloResult = firstVariable % secondVariable;
+
+console.log(moduloResult);
+
+//Zadanie 3
+var someNumber = "4";
+var someString = "425";
+let stringsResult = "";
+stringsResult = someNumber + someString;
+console.log(stringsResult);
+
+//Zadanie 4
+
+var someNumber = 425;
+var someString = "425";
+console.log(someNumber == someString);
+console.log(someNumber === someString);
+
+//Zadanie 5
+
+let counter = 30;
+
+console.log(counter);
+
+counter++;
+
+console.log(counter);
+
+counter--;
+
+console.log(counter);
+
+//Zadanie 6
+
+let variableOne=30;
+let variableTwo=20;
+let result=null;
+result=variableOne>variableTwo;
+console.log(result);
