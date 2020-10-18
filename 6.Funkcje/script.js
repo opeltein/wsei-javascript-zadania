9b771d46257a187745dbcf42f5f9a2d637e9d504
opeltein <61658879+opeltein@users.noreply.github.com>
2020-10-18 13:11:43 +0200

    Update script.js

---
+++ 6.Funkcje/script.js
@@ -0,0 +1,20 @@
+//Zadanie 2
+Napisz funkcję która przyjmuje dowolny parametr i wyświetla go w konsoli*/
+
+function printParam(str) {
+    console.log(str);
+}
+
+printParam('anyx');
+//Zad3
+
+const exampleArray = ["bla", "add", "fsa"];
+
+function returnArray(array) {
+    
+  return array;
+}
+
+console.log(returnArray(exampleArray));
+
+//Zad4
