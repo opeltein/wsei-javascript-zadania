e42afef548e633a5fb40cef1d269679d894edeef
opeltein <61658879+opeltein@users.noreply.github.com>
2020-12-02 12:59:33 +0100

    Update script.js

--- 2. Zmienne i stałe/script.js
+++ 2. Zmienne i stałe/script.js
@@ -1,8 +1,23 @@
 //Zadanie 1
+var nr = 20;
+var txt = "Tekst";
+var mix = 2+" Tekst";
+var prawda = true;
+var foo = null;
+
+console.log(txt);
+console.log(nr);
+console.log(mix);
+console.log(prawda);
+console.log(foo);
 
 
 //Zadanie 2
-
+var a = 2;
+var b = 5;
+var sum = 0;
+var sum = a+b;
+console.log(sum);
 
 
 // Zadanie 3
