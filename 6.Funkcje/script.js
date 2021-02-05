3b33147764d983ea5c8576ffb75900c976662f72
opeltein <61658879+opeltein@users.noreply.github.com>
2021-02-05 12:38:26 +0100

    Update script.js

--- 6.Funkcje/script.js
+++ 6.Funkcje/script.js
@@ -1,3 +1,9 @@
+//Zadanie 1
+function something(){
+	return "Udało się!";
+}
+console.log(something());
+
 //Zadanie 2
 Napisz funkcję która przyjmuje dowolny parametr i wyświetla go w konsoli*/
 
@@ -18,3 +24,15 @@ function returnArray(array) {
 console.log(returnArray(exampleArray));
 
 //Zad4
+var counter = 1;
+var smth = "hello world";
+
+function write(smth) {
+  console.log(smth);
+  counter++;
+  if (counter > 5) {
+    clearInterval(int);
+    console.log("Koniec.");
+  }
+}
+var int = setInterval(write, 3000, smth); 
